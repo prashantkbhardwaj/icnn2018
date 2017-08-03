23b12a10dd5afc80fce09227ec7aa26f29a40177
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2017-08-03 23:21:23 +0530

    test api

--- teqniHome/getData.php
+++ teqniHome/getData.php
@@ -1,11 +1,13 @@
 <?php require_once("includes/db_connection.php");?>
 <?php require_once("includes/functions.php");?>
 <?php
-	$query = "SELECT imgPath FROM volleyupload";
+	$query = "SELECT * FROM volleyupload";
 	$result = mysqli_query($conn, $query);
 	$i = 0;
+	$response = array();
 	while ($list = mysqli_fetch_assoc($result)) {
 		$response[$i]['url'] = $list['imgPath'];
+		$response[$i]['name'] = $list['uploader'];
 		$i = $i+1;
 	}
 	echo json_encode($response);
