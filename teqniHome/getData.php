7aee221e4dc5faa68ddbeaf65f5e8f85158e3a39
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2017-08-03 20:23:02 +0530

    bug found

--- teqniHome/getData.php
+++ teqniHome/getData.php
@@ -1,10 +1,10 @@
 <?php require_once("includes/db_connection.php");?>
 <?php require_once("includes/functions.php");?>
 <?php
-	$query = "SELECT * FROM volleyupload";
+	$query = "SELECT imgPath FROM volleyupload";
 	$result = mysqli_query($conn, $query);
 	$i = 0;
-	while ($list = mysqli_fetch_assoc()) {
+	while ($list = mysqli_fetch_assoc($result)) {
 		$response[$i]['url'] = $list['imgPath'];
 		echo $response[$i]['url']."<br>";
 		$i = $i+1;
