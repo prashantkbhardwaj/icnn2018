d7ecfca075f064def8f4aedd760dce595856dd16
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2017-08-04 11:08:12 +0530

    counter shifted down

--- index.php
+++ index.php
@@ -133,10 +133,6 @@
                             </a>
                             <h4 class="text-center"><strong>Industry Partner</strong></h4>
                        </div>
-                        <div class="col-md-12 text-center">
-                            Visitor Count 
-                            <div align=center><a href='http://www.counter12.com'><img src='http://www.counter12.com/img-B3bwDb1YYzd56d5a-13.gif' border='0' alt='free web counter'></a><script type='text/javascript' src='http://www.counter12.com/ad.js?id=B3bwDb1YYzd56d5a'></script></div>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -810,7 +806,8 @@
         <div class="copyright">
         © Created for VIT Chennai Campus</a>
        	<br>
-        2018 Year
+        Visitor Count
+        <div align=center><a href='http://www.counter12.com'><img src='http://www.counter12.com/img-B3bwDb1YYzd56d5a-13.gif' border='0' alt='free web counter'></a><script type='text/javascript' src='http://www.counter12.com/ad.js?id=B3bwDb1YYzd56d5a'></script></div>
         </div>
         <div class="footer-line"></div>
         </div><!-- /.row --> 
