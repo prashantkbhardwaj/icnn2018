f687584f7f192a0908324626d1f49cd70df34980
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2017-08-08 22:34:44 +0530

    api additions to levl

--- teqniHome/levelUpdate.php
+++ teqniHome/levelUpdate.php
@@ -6,8 +6,11 @@
         $level1 = $_POST['level1']; 
         $level2 = $_POST['level2'];
         $level3 = $_POST['level3'];
+        $level1opt = $_POST['level1opt'];
+        $level2opt = $_POST['level2opt'];
+        $level3opt = $_POST['level3opt'];
         
-        $query = "UPDATE levelNames SET level1 = '{$level1}', level2 = '{$level2}', level3 = '{$level3}' ";
+        $query = "UPDATE levelNames SET level1 = '{$level1}', level2 = '{$level2}', level3 = '{$level3}', level1opt = '{$level1opt}', level2opt = '{$level2opt}', level3 = '{$level3opt}' ";
         $result = mysqli_query($conn, $query);
 
         if ($result) {
