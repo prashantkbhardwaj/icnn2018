ffd62ce065d0f50854021ed6f0f49e2a8bae7efb
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2017-08-13 20:15:09 +0530

    added web service for qr code

---
+++ teqniHome/select.php
@@ -0,0 +1,12 @@
+<?php require_once("includes/db_connection.php");?>
+<?php require_once("includes/functions.php");?>
+
+<?php
+	$query = "SELECT * FROM volleyupload";
+	$result = mysqli_query($conn, $query);
+	confirm_query($result);
+    
+	while ($notification = mysqli_fetch_assoc($result)) {
+		echo "<div class='col-lg-3'><img src='".$notification['qrcode']."'></div>";
+	}		 
+?>
