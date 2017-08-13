a15c05350e79b317fcd67d48661e32cddc824af8
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2017-08-13 21:38:08 +0530

    updated api for data listening

---
+++ teqniHome/listen.php
@@ -0,0 +1,13 @@
+<?php require_once("includes/db_connection.php");?>
+<?php require_once("includes/functions.php");?>
+
+<?php
+	$query = "SELECT * FROM listenState";
+	$result = mysqli_query($conn, $query);
+	confirm_query($result);
+    $list = mysqli_fetch_assoc($result);
+
+	if ($list['state']==1) {
+		echo "<script>window.location.href = 'http://www.vit5icnn2018.com/teqniHome/test.php?data='".$list['data'].";</script>";
+	}		 
+?>
---
+++ teqniHome/updateState.php
@@ -0,0 +1,18 @@
+<?php require_once("includes/db_connection.php");?>
+<?php require_once("includes/functions.php");?>
+
+<?php
+    if ($_POST['data']!="") {
+        $data = $_POST['data']; 
+        
+        $query = "INSERT INTO listenData (data, state)";
+        $query .= " VALUES ('{$data}', '1')";
+        $result = mysqli_query($conn, $query);
+
+        if ($result) {
+            $response = array();
+            $response["success"] = true;  
+            echo json_encode($response);     
+        } 
+    }
+?>
