0fe90d766fb140de73ad4a6edb2fd58c2376de13
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2017-09-22 00:12:43 +0530

    create session update

--- teqniHome/createSession.php
+++ teqniHome/createSession.php
@@ -1,19 +1,20 @@
 <?php
- 	if($_SERVER['REQUEST_METHOD']=='POST'){
+	require_once("includes/db_connection.php");
  
-	    $uploader = $_POST['uploader'];
-	    $level1 = $_POST['level1'];
-	    $level2 = $_POST['level2'];
-	    $level3 = $_POST['level3'];
-	    $description = $_POST['description'];
-	    $sessionName = $_POST['sessionName'];
-	  	require_once("includes/db_connection.php");
-	 	$sql = "INSERT INTO volleyupload (uploader, level1, level2, level3, sessionName, description) VALUES ('{$uploader}', '{$level1}', '{$level2}', '{$level3}', '{$sessionName}', '{$description}')";
-		if(mysqli_query($conn,$sql)){
-		 	echo "Successfully Created";
-		}
-	 	mysqli_close($conn);
-	}else{
-	 	echo "Error";
-	}
+    $uploader = $_POST['uploader'];
+    $level1 = $_POST['level1'];
+    $level2 = $_POST['level2'];
+    $level3 = $_POST['level3'];
+    $description = $_POST['description'];
+    $sessionName = $_POST['sessionName'];
+  
+ 	$query = "INSERT INTO volleyupload (uploader, level1, level2, level3, sessionName, description) VALUES ('{$uploader}', '{$level1}', '{$level2}', '{$level3}', '{$sessionName}', '{$description}')";
+ 	$result = mysqli_query($conn, $query);
+ 	if ($result) {
+ 		echo "Successfully Created";
+ 	}else {
+ 		echo "Error";
+ 	}
+	
+ 	mysqli_close($conn);
 ?>
